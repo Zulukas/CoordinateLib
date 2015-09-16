42b1075b05bd47dc5d5a4aa4d28318cea1bccb50
Zulukas <1996nissan200sx@gmail.com>
2015-09-16 14:20:48 -0600

    fixed a typo in install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "Building and installing the coordinate library."
 make
 mv libcoordinate.so /usr/lib/
 cp coordinate.h /usr/include/
-echo "Finished installing.  To use library, add \'-lcoordinate\' as a flag"
+echo "Finished installing.  To use library, add '-lcoordinate' as a flag"
